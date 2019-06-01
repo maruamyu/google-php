10b51467139ba81a3f35748283b04deb3633e5f6
にゃー (mirai_iro) <mirai-iro@maruamyu.net>
2019-06-01 14:29:02 +0900

    ServiceAccountAuth using new OAuth2 library's JWT Bearer Grant

--- src/Maruamyu/Google/ServiceAccountAuth.php
+++ src/Maruamyu/Google/ServiceAccountAuth.php
@@ -2,22 +2,22 @@
 
 namespace Maruamyu\Google;
 
-use Maruamyu\Core\Http\Client as HttpClient;
-use Maruamyu\Core\Http\Message\QueryString;
 use Maruamyu\Core\OAuth2\AccessToken;
-use Maruamyu\Core\OAuth2\JsonWebKey;
-use Maruamyu\Core\OAuth2\JsonWebToken;
+use Maruamyu\Core\OAuth2\Client as VanillaOAuth2Client;
+use Maruamyu\Core\OAuth2\Settings as OAuth2Settings;
 
 /**
  * Google Service Account Authentication
  */
 class ServiceAccountAuth
 {
-    const ENDPOINT_URL = 'https://www.googleapis.com/oauth2/v4/token';
+    const DEFAULT_TOKEN_ENDPOINT_URL = 'https://www.googleapis.com/oauth2/v4/token';
 
-    private $authConfig;
+    /** @var ServiceAccount */
+    private $serviceAccount;
 
-    private $latestResponse;
+    /** @var VanillaOAuth2Client */
+    private $oAuth2Client;
 
     /**
      * @param string|array $authConfigSrc auth parameter or JSON file path
@@ -39,10 +39,17 @@ class ServiceAccountAuth
         } else {
             throw new \InvalidArgumentException('invalid auth config data: ' . gettype($authConfigSrc));
         }
-        if ($authConfig['type'] !== 'service_account') {
-            throw new \RuntimeException('invalid type=' . $authConfig['type']);
+
+        $this->serviceAccount = new ServiceAccount($authConfig);
+
+        $oAuth2Settings = new OAuth2Settings();
+        $oAuth2Settings->clientId = $authConfig['client_id'];
+        if (isset($authConfig['token_uri'])) {
+            $oAuth2Settings->tokenEndpoint = $authConfig['token_uri'];
+        } else {
+            $oAuth2Settings->tokenEndpoint = static::DEFAULT_TOKEN_ENDPOINT_URL;
         }
-        $this->authConfig = $authConfig;
+        $this->oAuth2Client = new VanillaOAuth2Client($oAuth2Settings);
     }
 
     /**
@@ -50,58 +57,28 @@ class ServiceAccountAuth
      */
     public function getClientId()
     {
-        return strval($this->authConfig['client_id']);
+        return $this->serviceAccount->getClientId();
     }
 
     /**
      * @param string[] $scopes list of scopes
      * @param int $expireSec expire(seconds)
-     * @param string $sub sub account mail address
+     * @param string $subject account mail address
      * @return AccessToken|null AccessToken (return null if failed)
      * @throws \Exception if invalid auth-config
      */
-    public function fetchAccessToken(array $scopes, $expireSec = 3600, $sub = '')
+    public function fetchAccessToken(array $scopes, $expireSec = 3600, $subject = '')
     {
-        $scope = join(' ', $scopes);
+        $jsonWebKey = $this->serviceAccount->getJsonWebKey();
+        $issuer = $this->serviceAccount->getClientEmail();
+
         $nowTimestamp = time();
         $expireAtTimestamp = $nowTimestamp + $expireSec;
 
-        $jsonWebKey = JsonWebKey::createFromRsaPrivateKey($this->authConfig['private_key'],
-            null, $this->authConfig['private_key_id'], 'RS256');
-
-        $jwtClaimSet = [
-            'iss' => $this->authConfig['client_email'],
-            'scope' => $scope,
-            'aud' => static::ENDPOINT_URL,
-            'exp' => $expireAtTimestamp,
+        $optionalParameters = [
             'iat' => $nowTimestamp,
         ];
-        if ($sub) {
-            $jwtClaimSet['sub'] = $sub;
-        }
 
-        $queryParameters = [
-            'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-            'assertion' => JsonWebToken::build($jwtClaimSet, $jsonWebKey),
-        ];
-        $requestBody = QueryString::build($queryParameters);
-
-        $httpClient = new HttpClient();
-        $response = $httpClient->request('POST', static::ENDPOINT_URL, ['body' => $requestBody]);
-        $this->latestResponse = $response;
-        if ($response->statusCodeIsOk() == false) {
-            return null;
-        }
-        $responseBody = strval($response->getBody());
-        if (strlen($responseBody) < 1) {
-            return null;
-        }
-
-        $tokenData = json_decode($responseBody, true);
-        if (empty($tokenData)) {
-            return null;
-        }
-        $tokenData['scope'] = $scope;
-        return new AccessToken($tokenData);
+        return $this->oAuth2Client->requestJwtBearerGrant($jsonWebKey, $issuer, $subject, $expireAtTimestamp, $scopes, $optionalParameters);
     }
 }
